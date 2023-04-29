8eec1ccac2642b43b33493e7bb11aef83ecbb667
Vitaly <vitalic.web@gmail.com>
2023-04-29 18:19:04 +0300

    Fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vuejs-accessibility/anchor-has-content': 'off',
   },
 };
