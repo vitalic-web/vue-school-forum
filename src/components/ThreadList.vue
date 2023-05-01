8ebfa1af27f0c09a274005e2ef9776e527f9e8f3
Vitaly <vitalic.web@gmail.com>
2023-05-01 13:31:21 +0300

    Add ThreadList component

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="col-full">
+    <div class="thread-list">
+      <h2 class="list-title">Threads</h2>
+      <div v-for="thread in threads" :key="thread.id" class="thread">
+        <div>
+          <p>
+            <a href="#">{{ thread.title }}</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+          </p>
+        </div>
+        <div class="activity">
+          <p class="replies-count">
+            {{ thread.posts.length }} replies
+          </p>
+          <img class="avatar-medium" :src="userById(thread.userId).avatar" alt="">
+          <div>
+            <p class="text-xsmall">
+              <a href="#">{{ userById(thread.userId).name }}</a>
+            </p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import sourceData from '@/data.json';
+
+defineProps({
+  threads: {
+    type: Array,
+    required: true,
+  },
+});
+
+const posts = ref(sourceData.posts);
+const users = ref(sourceData.users);
+
+const postById = (postId) => posts.value.find((p) => p.id === postId);
+const userById = (userId) => users.value.find((u) => u.id === userId);
+</script>
