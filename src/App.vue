e8f1e055e7af1e94bc53257fb42467e3faa45c06
Vitaly <vitalic.web@gmail.com>
2023-05-12 16:10:22 +0300

    Create TheNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  <TheNavbar />
   <div class="container">
     <router-view />
   </div>
 </template>
 
 <script setup>
+import TheNavbar from '@/components/TheNavbar.vue';
 </script>
 
 <style lang="scss">
