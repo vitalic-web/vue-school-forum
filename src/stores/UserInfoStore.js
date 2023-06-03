f8f7c2820b9b5eb781ed39a70a69be4748db2529
Vitaly <vitalic.web@gmail.com>
2023-06-03 15:24:43 +0300

    Refactor AuthStore to UserInfoStore

---
+++ src/stores/UserInfoStore.js
@@ -0,0 +1,39 @@
+import { defineStore, storeToRefs } from 'pinia';
+import useUsersStore from '@/stores/UsersStore';
+import usePostsStore from '@/stores/PostsStore';
+import useThreadsStore from '@/stores/ThreadsStore';
+
+const useUserInfoStore = defineStore('UserInfoStore', {
+  state: () => ({
+    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+  }),
+  getters: {
+    authUser: (state) => {
+      const { users } = storeToRefs(useUsersStore());
+      const { posts } = storeToRefs(usePostsStore());
+      const { threads } = storeToRefs(useThreadsStore());
+      const user = users.value.find((userEl) => userEl.id === state.authId);
+      if (!user) return null;
+      return {
+        ...user,
+        // get to use authUser.posts instead of authUser.getPosts()
+        get posts() {
+          return posts.value.filter((post) => post.userId === user.id);
+        },
+        get postsCount() {
+          return this.posts.length;
+        },
+        get threads() {
+          return threads.value.filter((thread) => thread.userId === user.id);
+        },
+        get threadsCount() {
+          return this.threads.length;
+        },
+      };
+    },
+  },
+  actions: {
+  },
+});
+
+export default useUserInfoStore;
